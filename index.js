18cf78ca4cba73a6fe212a3c9f4808d2ccdad1f9
jaiswarvishal <121937798+jaiswarvishal@users.noreply.github.com>
2023-01-25 14:19:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-let randomImageSource2 = "Images/dice" +randomNumber2 + ".png";
+let randomImageSource2 = "Images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If Player 1 wins
@@ -19,6 +19,7 @@ if (randomNumber1 > randomNumber2){
 else if(randomNumber2 > randomNumber1){
     document.querySelector("h1").innerHTML = "🚩Player 2 Wins!"
 }
+//If Draw!
 else {
     document.querySelector("h1").innerHTML = "Draw!😃"
 }
