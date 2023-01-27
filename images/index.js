08208566a567cded45d6f251bad77709d14da6e3
jaiswarvishal <121937798+jaiswarvishal@users.noreply.github.com>
2023-01-27 16:15:38 +0530

    Images

---
+++ images/index.js
@@ -0,0 +1,16 @@
+let randomNumber1 = Math.floor(Math.random() * 6 ) + 1; 
+//1 - 6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+
+var randomImageSource = "images/" + randomDiceImage;
+
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
